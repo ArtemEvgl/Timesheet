41893df638069340d51e14de14888c815830606e
ArtemEvgl <Usbhawk5@gmail.com>
2020-12-19 23:08:38 +0300

    edit logic for get employee's report

--- Timesheet.Application/Services/ReportService.cs
+++ Timesheet.Application/Services/ReportService.cs
@@ -6,7 +6,7 @@ namespace Timesheet.Application.Services
 {
     public class ReportService
     {
-        private const decimal MAX_WORKING_HOURS_PER_MONTH = 160m;
+        private const int MAX_WORKING_HOURS_PER_MONTH = 160;
 
         private readonly ITimesheetRepository _timesheetRepository;
         private readonly IEmployeeRepository _employeeRepository;
@@ -22,8 +22,34 @@ namespace Timesheet.Application.Services
             var employee = _employeeRepository.GetEmployee(lastName);
             var timeLogs = _timesheetRepository.GetTimeLogs(employee.LastName);
 
-            var hours = timeLogs.Sum(x => x.WorkingHours);
-            var bill = (hours / MAX_WORKING_HOURS_PER_MONTH) * employee.Salary;
+            //var hours = timeLogs.Sum(x => x.WorkingHours);
+            //var bill = (hours / MAX_WORKING_HOURS_PER_MONTH) * employee.Salary;
+
+            int monthHours = timeLogs[0].WorkingHours;
+            decimal billPerHour = employee.Salary / 160;
+            decimal bill = monthHours * billPerHour;                       
+            for (int i = 1; i < timeLogs.Length; i++)
+            {
+                int dayHours = timeLogs[i].WorkingHours;
+                if (timeLogs[i].Date.Month != timeLogs[i - 1].Date.Month)
+                {
+                    monthHours = 0;
+                }
+                monthHours += dayHours;
+                if (monthHours <= 160)
+                {
+                    bill += timeLogs[i].WorkingHours * billPerHour;
+                } else if (monthHours < 168)
+                {
+                    int overWorkHours = monthHours - MAX_WORKING_HOURS_PER_MONTH;
+                    int simpleWorkHours = dayHours - overWorkHours;
+                    bill += simpleWorkHours * billPerHour;
+                    bill += overWorkHours * billPerHour * 2;
+                } else
+                {
+                    bill += timeLogs[i].WorkingHours * billPerHour * 2;
+                }
+            }
 
             return new EmployeeReport
             {
--- Timesheet.Tests/ReportServiceTests.cs
+++ Timesheet.Tests/ReportServiceTests.cs
@@ -10,7 +10,7 @@ namespace Timesheet.Tests
     class ReportServiceTests
     {
         [Test]
-        public void GetEmployeeReport_ShouldReturnReport()
+        public void GetEmployeeReport_ShouldReturnReportPerOneMonth()
         {
             //arrange
             var timesheetRepositoryMock = new Mock<ITimesheetRepository>();
@@ -66,6 +66,68 @@ namespace Timesheet.Tests
             Assert.IsNotNull(result);
             Assert.AreEqual(expectedLastName, result.LastName);
 
+            Assert.IsNotNull(result.TimeLogs);
+            Assert.IsNotEmpty(result.TimeLogs);
+
+            Assert.AreEqual(expectedTotal, result.Bill);
+        }
+
+        [Test]
+        public void GetEmployeeReport_ShouldReturnReportPerSeveralMonth()
+        {
+            //arrange
+            var timesheetRepositoryMock = new Mock<ITimesheetRepository>();
+            var employeeRepositoryMock = new Mock<IEmployeeRepository>();
+            var expectedLastName = "Иванов";
+            // ставка за час = 60000 / 160 = 375
+            //  20 * 8 * 375 + 10 * 8 * 375 * 2(дни переработки) + 1 * 375 * 2(1 час переработки в 1 дне) + 5 * 8 * 375 (рабочие дни в след месяце)
+            var expectedTotal = 135750m; 
+            
+
+            employeeRepositoryMock
+                .Setup(x => x.GetEmployee(It.Is<string>(x => x == expectedLastName)))
+                .Returns(() => new StaffEmployee
+                {
+                    LastName = expectedLastName,
+                    Salary = 60000
+                })
+                .Verifiable();
+
+            timesheetRepositoryMock
+                .Setup(x => x.GetTimeLogs(It.Is<string>(x => x == expectedLastName)))
+                .Returns(() =>
+                {
+                    TimeLog[] timeLogs = new TimeLog[35]; 
+                    DateTime dateTime = new DateTime(2020, 11, 1);
+                    timeLogs[0] = new TimeLog
+                    {
+                        LastName = expectedLastName,
+                        Comment = Guid.NewGuid().ToString(),
+                        Date = dateTime,
+                        WorkingHours = 9
+                    };
+                    for (int i = 1; i < timeLogs.Length; i++)
+                    {
+                        dateTime = dateTime.AddDays(1);
+                        timeLogs[i] = new TimeLog { 
+                            LastName = expectedLastName, 
+                            Comment = Guid.NewGuid().ToString(), 
+                            Date = dateTime,
+                            WorkingHours = 8
+                        };                        
+                    }
+                    return timeLogs;
+                })
+                .Verifiable();
+
+            var service = new ReportService(timesheetRepositoryMock.Object, employeeRepositoryMock.Object);
+            //act
+            var result = service.GetEmployeeReport(expectedLastName);
+            //assert
+
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(expectedLastName, result.LastName);
             Assert.IsNotNull(result.TimeLogs);
             Assert.IsNotEmpty(result.TimeLogs);
 
