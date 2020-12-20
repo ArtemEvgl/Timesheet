25a9467b7e947282c3232a172761ccd9162b619f
Trufanov Roman <pingvin1308@gmail.com>
2020-12-20 14:09:57 +0300

    feat (ReportService): rework bill calculation
    Now we calculate the bill for each day logs

--- Timesheet.Domain/Models/EmployeeReport.cs
+++ Timesheet.Domain/Models/EmployeeReport.cs
@@ -12,6 +12,11 @@ namespace Timesheet.Domain.Models
     /// </summary>
     public class EmployeeReport
     {
+        public EmployeeReport()
+        {
+            TimeLogs = new List<TimeLog>();
+        }
+
         public string LastName { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
