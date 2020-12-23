cfa0eccdda7ed14672cc578f5af9a3804a2df88f
ArtemEvgl <Usbhawk5@gmail.com>
2020-12-23 22:13:26 +0300

    add EmployeeServiceTest

---
+++ Timesheet.Tests/EmployeeServiceTest.cs
@@ -0,0 +1,60 @@
+﻿
+
+using Moq;
+using NUnit.Framework;
+using Timesheet.Application.Services;
+using Timesheet.Domain;
+using Timesheet.Domain.Models;
+
+namespace Timesheet.Tests
+{
+    
+    class EmployeeServiceTest
+    {
+
+        [Test]
+        [TestCase("Иванов", 20000)]
+        [TestCase("Петров", 30000)]
+        [TestCase("Сидоров", 40000)]
+        public void Add_ShouldReturnTrue(string lastName, int salary)
+        {
+            //arrange
+            var staffEmployee = new StaffEmployee() { LastName = lastName, Salary = salary };
+            
+            var employeeRepository = new Mock<IEmployeeRepository>();
+            employeeRepository.Setup(x => x.AddEmployee(staffEmployee)).Verifiable();
+
+            var service = new EmployeeService(employeeRepository.Object);
+
+            //act
+            var result = service.AddEmployee(staffEmployee);
+
+            //assert
+            employeeRepository.Verify(x => x.AddEmployee(staffEmployee), Times.Once);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        [TestCase("Иванов", 0)]
+        [TestCase("Иванов", -1000)]
+        [TestCase("", 30000)]
+        [TestCase(null, 40000)]
+        public void Add_ShouldReturnFalse(string lastName, int salary)
+        {
+            //arrange
+            var staffEmployee = new StaffEmployee() { LastName = lastName, Salary = salary };
+
+            var employeeRepository = new Mock<IEmployeeRepository>();
+            employeeRepository.Setup(x => x.AddEmployee(staffEmployee)).Verifiable();
+
+            var service = new EmployeeService(employeeRepository.Object);
+
+            //act
+            var result = service.AddEmployee(staffEmployee);
+
+            //assert
+            employeeRepository.Verify(x => x.AddEmployee(staffEmployee), Times.Never);
+            Assert.IsFalse(result);
+        }
+    }
+}
