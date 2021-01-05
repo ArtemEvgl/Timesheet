24343e881915dc6267eba425c72892b9d36175ba
ArtemEvgl <Usbhawk5@gmail.com>
2021-01-05 15:32:14 +0300

    create (ChiefEmployeeController, StaffEmployeeController, FrealanceremployeeController): create for receive dates about several employees

---
+++ Timesheet.Api/Controllers/ChiefEmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Timesheet.Domain;
+using Timesheet.Domain.Models;
+
+namespace Timesheet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChiefEmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public ChiefEmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+       
+        [HttpPost]
+        public ActionResult<bool> Add(ChiefEmployee ChiefEmployee)
+        {
+            return Ok(_employeeService.AddEmployee(ChiefEmployee));
+        }
+        
+    }
+}
