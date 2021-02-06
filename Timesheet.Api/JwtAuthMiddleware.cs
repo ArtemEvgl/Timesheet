7c97b1476366fc3e1853010a72b59b8dec30fd89
Trufanov Roman <pingvin1308@gmail.com>
2021-02-06 14:13:51 +0300

    feat (AuthService): move secrets to configuration

--- Timesheet.Api/JwtAuthMiddleware.cs
+++ Timesheet.Api/JwtAuthMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
@@ -12,15 +13,13 @@ namespace Timesheet.Api
     public class JwtAuthMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
 
-        public JwtAuthMiddleware(RequestDelegate next)
+        public JwtAuthMiddleware(RequestDelegate next, IConfiguration configuration)
         {
             _next = next;
+            _configuration = configuration;
         }
-
-        /*
-         eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJMYXN0TmFtZSI6IlRlc3RMYXN0TmFtZSIsIm5iZiI6MTYxMDg3ODc5OCwiZXhwIjoxNjEwODgyMzk4LCJpYXQiOjE2MTA4Nzg3OTgsImF1ZCI6IkNoaWVmIn0.JuxUXaY5_JK89aZlyCk2Etgd-7kyCTizKok-FTbsPdM
-         */
 
         public async Task Invoke(HttpContext context)
         {
@@ -29,7 +28,13 @@ namespace Timesheet.Api
 
             if (authHeader != null)
             {
-                var secret = "secret secret secret secret secret";
+                var secret = _configuration.GetSection("jwtSecret").Value;
+
+                if(string.IsNullOrWhiteSpace(secret))
+                {
+                    throw new Exception("Set up your secret");
+                }
+
                 var key = Encoding.UTF8.GetBytes(secret);
 
                 var token = authHeader.Split(" ").Last();
--- Timesheet.Domain/Models/Issue.cs
+++ Timesheet.Domain/Models/Issue.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Timesheet.Domain.Models
+﻿namespace Timesheet.Domain.Models
 {
     public class Issue
     {
